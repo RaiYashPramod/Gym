2b68f163cfc0f8350fe2b4cb5fe89fff72d6cac5
Rai Yash Pramod Anita <raiyashpramod16@gmail.com>
2022-07-27 09:24:45 +0530

    update and delete routes working

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -12,18 +12,19 @@ const getWorkout = async (req, res) => {
     if(!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({ error: 'NO SUCH WORKOUT' });
     }
-
+    
     const workout = await Workout.findById(id);
-
     if(!workout) {
         return res.status(404).json({ error: 'No such workout was found in the database' })
     }
+    return res.status(200).send(workout);
 }
 
 const createWorkout = async (req, res) => {
     const {title, reps, load} = req.body;
     try {
         const workout = await Workout.create({ title, reps, load });
+        console.log(workout);
         return res.status(200).json(workout)
     } catch (error) {
         res.status(400).json({ error: error.mssg });
