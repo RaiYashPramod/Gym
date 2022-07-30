3172baca27882989b6e0feb6ad8c60b727c7177b
Rai Yash Pramod Anita <raiyashpramod16@gmail.com>
2022-07-30 13:01:07 +0530

    used context hook to refresh and add workout whenever a new workout is added

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -2,8 +2,9 @@ const WorkoutDetails = ({ workout }) => {
     return(
         <div className="workout-details">
             <h4>{workout.title}</h4>
-            <p><strong>Loads (kg):</strong>{workout.load}</p>
-            <p><strong>Reps:</strong>{workout.reps}</p>
+            <p><strong>Loads (kg):</strong> {workout.load}</p>
+            <p><strong>Reps:</strong> {workout.reps}</p>
+            <p>{workout.createdAt}</p>
         </div>
     )
 }
