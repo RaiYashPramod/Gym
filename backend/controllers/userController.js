1fd154dfd3888e29f6b0d69a94717676731355a4
Rai Yash Pramod Anita <raiyashpramod16@gmail.com>
2022-08-06 13:14:55 +0530

    jwt applied

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModels')
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.JWT_SECRET, {expiresIn: '3d'})
+}
 
 // login a user
 const loginUser = async (req, res) => {
@@ -11,8 +17,10 @@ const loginUser = async (req, res) => {
   
     try {
       const user = await User.signup(email, password)
+      
+      const token = createToken(user._id)
   
-      res.status(200).json({email, user})
+      res.status(200).json({email, token})
     } catch (error) {
       res.status(400).json({error: error.message})
     }
